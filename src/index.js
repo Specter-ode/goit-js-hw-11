ba1e9f9829e09d7d328777dbf79fc177e678b05b
Vlad Shchelkunov <specter.ode@gmail.com>
2022-07-10 14:53:03 +0300

    рабочий вариант
    еще надо добавить плавный и бесконечный скролл

--- src/index.js
+++ src/index.js
@@ -10,23 +10,27 @@ const galleryContainer = document.querySelector('.gallery');
 const formEl = document.querySelector('#search-form');
 const loadMoreBtnEl = document.querySelector('.load-more');
 let page = 1;
+let searchValue = '';
+const perPage = 40;
 
 const onBtnSubmit = async e => {
   e.preventDefault();
-  const searchValue = e.target.elements.searchQuery.value.trim().toLowerCase();
+  searchValue = e.target.elements.searchQuery.value.trim().toLowerCase();
   try {
     clearFields();
-    const { data } = await startFetchPosts(searchValue, page);
+    const { data } = await startFetchPosts(searchValue, page, perPage);
     if (searchValue === '') {
+      alertNoEmptySearch();
       loadMoreBtnEl.classList.add('is-hidden');
-    return;
+      return;
     } else if (data.totalHits === 0) {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+      alertNoImagesFound();
       loadMoreBtnEl.classList.add('is-hidden');
+      return;
     } else {
       galleryContainer.innerHTML = createPostCard(data.hits);
+      makeSlider();
+      alertAmountImagesFound(data);
       loadMoreBtnEl.classList.remove('is-hidden');
     }
     console.log(data);
@@ -36,26 +40,51 @@ const onBtnSubmit = async e => {
   }
 };
 
+const onLoadMoreBtnElClick = async () => {
+  try {
+    page += 1;
+    const { data } = await startFetchPosts(searchValue, page, perPage);
+    const totalPages = Math.ceil(data.totalHits / perPage);
+    if (page > totalPages) {
+      alertEndOfSearch();
+      return;
+    }
+    galleryContainer.insertAdjacentHTML('beforeend', createPostCard(data.hits));
+    loadMoreBtnEl.classList.remove('is-hidden');
+    makeSlider();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+formEl.addEventListener('submit', onBtnSubmit);
+loadMoreBtnEl.addEventListener('click', onLoadMoreBtnElClick);
+
 function clearFields() {
   galleryContainer.innerHTML = '';
 }
 
-const onLoadMoreBtnElClick = async e => {
-try {
-  page += 1;
-  const { data } = await continueFetchPosts(page);
-  galleryContainer.insertAdjacentHTML('beforeend', createPostCard(data.hits));
-  loadMoreBtnEl.classList.remove('is-hidden');
-} catch (error) {
-    console.log(error);
+function makeSlider() {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+  }).refresh();
 }
+function alertAmountImagesFound(data) {
+  Notify.success(`Hooray! We found ${data.totalHits} images.`);
 }
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  captionPosition: 'bottom',
-});
+function alertNoEmptySearch() {
+  Notify.failure('The search cannot be empty. Please make correct query.');
+}
 
-formEl.addEventListener('submit', onBtnSubmit);
-loadMoreBtnEl.addEventListener('click', onLoadMoreBtnElClick);
+function alertNoImagesFound() {
+  Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function alertEndOfSearch() {
+  Notify.info("We're sorry, but you've reached the end of search results.");
+}
--- src/js/fetchPosts.js
+++ src/js/fetchPosts.js
@@ -3,35 +3,12 @@ import axios from 'axios';
 const API_KEY = '28531485-c2ddecdafcd3c65a85d6ac636';
 const BASE_URL = 'https://pixabay.com/api/';
 
-// export const fetchPost = (post, page) => {
-//   return fetch(`${BASE_URL}?key=${API_KEY}&q=${post}&page=${page}&per_page=40&image_type=photo&orientation=gorizontal&safesearch=true`)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   })
-// }
-
-export const startFetchPosts = (query, page) => {
+export const startFetchPosts = (query, page, limitPerPage) => {
   return axios.get(`${BASE_URL}`, {
     params: {
       q: `${query}`,
       key: `${API_KEY}`,
-      per_page: 40,
-      page: `${page}`,
-      image_type: 'photo',
-      orientation: 'gorizontal',
-      safesearch: true,
-    },
-  });
-};
-
-export const continueFetchPosts = page => {
-  return axios.get(`${BASE_URL}`, {
-    params: {
-      key: `${API_KEY}`,
-      per_page: 12,
+      per_page: `${limitPerPage}`,
       page: `${page}`,
       image_type: 'photo',
       orientation: 'gorizontal',
